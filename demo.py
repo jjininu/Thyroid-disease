160348092d9d37fd8598c5e8ebdf27fe1380bd8c
Junior Eshetu <76874506+jjininu@users.noreply.github.com>
2022-08-22 21:30:52 -0400

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,22 @@
+from insurance.pipeline.pipeline import Pipeline
+from insurance.exception import CustomException
+from insurance.logger import logging
+from insurance.config.configuration import Configuartion
+from insurance.component.data_transformation import DataTransformation
+import os
+def main():
+    try:
+        config_path = os.path.join("config","config.yaml")
+        pipeline = Pipeline(Configuartion(config_file_path=config_path))
+        #pipeline.run_pipeline()
+        pipeline.start()
+        logging.info("main function execution completed.")
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+
+
+
+if __name__=="__main__":
+    main()
+
